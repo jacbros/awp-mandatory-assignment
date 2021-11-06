104530b5938f7fa2f29d54cb3b60db4ddc3e4bc4
jacbros <jacbros1@gmail.com>
2021-11-06 17:49:30 +0100

    Added guote, guotes and AddGuote without functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,56 @@
 import { useEffect, useState } from "react";
+import { Router, Link } from "@reach/router";
+
+import Guotes from "./Guotes";
+import Guote from "./Guote";
+
+import "./style.css";
 const API_URL = process.env.REACT_APP_API;
 
 function App() {
-  const [data, setData] = useState([]);
+  const [guotes, setGuotes] = useState([]);
 
   useEffect(() => {
-    async function getData() {
-      const url = `${API_URL}/recipes`;
+    async function getGuotes() {
+      const url = `${API_URL}/guotes`;
       const response = await fetch(url);
       const data = await response.json();
-      setData(data);
+      setGuotes(data);
     }
-    getData();
+    getGuotes();
   }, []);
+
+  function getGuote(id) {
+    return guotes.find((guote) => guote.id === parseInt(id));
+  }
+
+  function addGuote(guote, author) {
+    const newGuote = {
+      id: guotes.length + 1,
+      guote: guote,
+      author: author,
+    };
+
+    fetch(API_URL, {
+      // PUT instead of POST because we overwrite the whole bin with a new version
+      // https://jsonbin.io/api-reference/v3/bins/update
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // Simple version where we overwrite the entire "database" store with a new list
+      body: JSON.stringify([...guotes, newGuote]),
+    })
+      .then((response) => response.json())
+      .then((data) => setGuotes(data.record))
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
   return (
     <>
-      <h1>MERN App!</h1>
-      <p>Data from server:</p>
-      <ul>
-        {data.map((item) => {
-          return (
-            <li key={item._id}>
-              <strong>{item.title}</strong> (<code>{item._id}</code>)
-            </li>
-          );
-        })}
-      </ul>
+      
     </>
   );
 }
